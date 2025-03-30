3c44602030be94b3f55afcaef28379c908cd2807
Cheemx <chinmaymahajan999@gmail.com>
2025-03-30 06:12:17 +0000

    2 finished: player movements added

--- main.py
+++ main.py
@@ -1,19 +1,36 @@
-from pygame.locals import *
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock_obj = pygame.time.Clock()
+    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return 
+        
+        updatable.update(dt)
+        
         screen.fill("black")
+
+        for player in drawable:
+            player.draw(screen)
+            
         pygame.display.flip()
+        
+        dt = clock_obj.tick(60) / 1000
+        
 
 if __name__ == "__main__":
     main()
